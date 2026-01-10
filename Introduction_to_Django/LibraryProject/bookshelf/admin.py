bd65aed1dbef028deef3de827f9ba2be18fe1009
Ehab Yasser <ehabyasser679@gmail.com>
2026-01-10 12:06:14 +0200

    Fix BookAdmin registration and add list_filter

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from .models import Book
 
 
-@admin.register(book)
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
     list_per_page = 10
     list_editable = ('author', 'publication_year')
     ordering = ('title',)
+
+
 

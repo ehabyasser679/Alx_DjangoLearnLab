d1064ab2304792cdc8b28a5e34ff1080d8421669
Ehab Yasser <ehabyasser679@gmail.com>
2026-01-18 21:12:17 +0200

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,28 +1,12 @@
-"""
-URL configuration for djangomodels project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
-from  relationship_app import views
-from .views import list_books", "LibraryDetailView
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from relationship_app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('relationship_app/', include('relationship_app.urls')),
     path('libraries/', views.LibrarylistView.as_view(), name='library-list'),
     path('libraries/<int:pk>/', views.LibrarydetailView.as_view(), name='library-detail'),
+    path('books/', views.all_books_view, name='book-list'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
+    path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
 ]
-

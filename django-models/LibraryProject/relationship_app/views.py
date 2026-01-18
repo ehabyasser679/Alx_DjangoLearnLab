46659b19213a66eaa8210c5a841563249c5322ca
Ehab Yasser <ehabyasser679@gmail.com>
2026-01-18 22:00:28 +0200

    Implement admin access control in views
    Added admin_required decorator and admin_view function to restrict access to admin users.

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, ListView, DetailView
-from .models import Library, Book
+from .models import Library, Book, Admin
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from functools import wraps
 
 # Create your views here.
 class LibrarylistView(ListView, TemplateView):
@@ -27,4 +30,36 @@ def register(request):
 def all_books_view(request):
     books = Book.objects.all()
     return render(request, "relationship_app/book_list.html", {"books": books})
+
+def admin_required(view_func):
+    """Decorator to check if user has Admin role"""
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('login')
+        try:
+            admin_instance = Admin.objects.get(user=request.user)
+        except Admin.DoesNotExist:
+            return HttpResponseForbidden("You do not have permission to access this page. Admin role required.")
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
+@login_required
+@admin_required
+def admin_view(request):
+    """Admin view that only users with Admin role can access"""
+    try:
+        admin_instance = Admin.objects.get(user=request.user)
+        libraries = Library.objects.all()
+        books = Book.objects.all()
+        context = {
+            'admin': admin_instance,
+            'libraries': libraries,
+            'books': books,
+            'total_libraries': libraries.count(),
+            'total_books': books.count(),
+        }
+        return render(request, "relationship_app/admin.html", context)
+    except Admin.DoesNotExist:
+        return HttpResponseForbidden("You do not have permission to access this page. Admin role required.")
 

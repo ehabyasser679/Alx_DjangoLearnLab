db41f8b29778f554ef1439433708e295e4d65691
Ehab Yasser <ehabyasser679@gmail.com>
2026-02-14 22:19:08 +0200

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import CustomUserCreationForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from .forms import CustomUserCreationForm
+from .forms import UserCreationForm
 
 
 def registeration(request):
     if request.method == "POST":
-        CustomUserCreationForm(request.POST)
+        UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect("login")
     else:
-        CustomUserCreationForm()
+        UserCreationForm()
         return render(request, "blog/register.html", {"form": form})
 

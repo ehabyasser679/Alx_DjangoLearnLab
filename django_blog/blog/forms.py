734f5f5642c50bd4f87c89a9c0f77b70dceb7f24
Ehab Yasser <ehabyasser679@gmail.com>
2026-02-14 22:44:41 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -9,7 +9,7 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ('username', 'email', 'password1', 'password2')
 
     def save(self, commit = True):
-        user = super(),save(commit = False)
+        user = super().save(commit = False)
         user.email = self.cleaned_data['email']
         if commit:
             user.save()

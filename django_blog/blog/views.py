8aea58462fa3769e2bb3d446846d41a38cbfaac0
Ehab Yasser <ehabyasser679@gmail.com>
2026-02-14 22:14:08 +0200

    Add user registration view in views.py
    Implement user registration view with form handling.

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import CustomUserCreationForm
+from django.contrib.auth import login
+from .forms import CustomUserCreationForm
 
-# Create your views here.
+
+def registeration(request):
+    if request.method == "POST":
+        CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("login")
+    else:
+        CustomUserCreationForm()
+        return render(request, "blog/register.html", {"form": form})
+

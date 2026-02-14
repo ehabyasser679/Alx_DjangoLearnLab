62c87c825c52c08ce5db9437ca3423da383e0970
Ehab Yasser <ehabyasser679@gmail.com>
2026-02-15 01:19:34 +0200

    Add blog post URLs to urlpatterns

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,17 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlogDeleteView
+
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    
     path('register/', views.register, name='register'),
-    path("profile/", views.profile, name= "profile")
+    path("profile/", views.profile, name= "profile"),
+    path("posts/", BlogListView.as_view(), name= "posts"),
+    path("posts/<int:pk>/", BlogDetailView.as_view(), name= "post_detail"),
+    path("posts/new/", BlogCreateView.as_view(), name= "post_new"),
+    path("posts/<int:pk>/edit/", BlogUpdateView.as_view(), name= "post_edit"),
+    path("posts/<int:pk>/delete/", BlogDeleteView.as_view(), name= "post_delete"),
 ]

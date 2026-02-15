da9a5839f60f62178ab90fcf0fc3cca465635509
Ehab Yasser <ehabyasser679@gmail.com>
2026-02-15 23:41:59 +0200

    Refactor urlpatterns for blog app

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -13,8 +13,9 @@ urlpatterns = [
     path("post/<int:pk>/", BlogDetailView.as_view(), name= "post_detail"),
     path("post/new/", BlogCreateView.as_view(), name= "post_new"),
     path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name= "post_edit"),
-    path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name= "post_delete"),
+    path("post/<int:pk>/update/", BlogDeleteView.as_view(), name= "post_delete"),
     path('post/<int:pk>/comment/', views.CommentCreateView.as_view(), name='add_comment'),
     path('comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='comment_edit'),
     path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
 ]
+

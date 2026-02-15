c9d29ab6b51b5cecf1fc9eaee012d41d335c75c6
Ehab Yasser <ehabyasser679@gmail.com>
2026-02-15 23:44:02 +0200

    Add CommentForm to handle blog comments
    Added CommentForm for creating comments in the blog.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Comment
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField()
@@ -14,3 +15,11 @@ class CustomUserCreationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3}),
+        }

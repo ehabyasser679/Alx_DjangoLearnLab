8d27cce0624971fec192a10ce4c2ded5264729ca
Ehab Yasser <ehabyasser679@gmail.com>
2026-02-16 00:06:26 +0200

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Comment
+from .models import Comment, Post
+from taggit.forms import TagWidget
+from taggit.managers import TaggableManager
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField()
@@ -12,6 +14,7 @@ class CustomUserCreationForm(UserCreationForm):
     def save(self, commit = True):
         user = super().save(commit = False)
         user.email = self.cleaned_data['email']
+        user.tags = TaggableManager()
         if commit:
             user.save()
         return user
@@ -22,4 +25,12 @@ class CommentForm(forms.ModelForm):
         fields = ['content']
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3}),
+        }
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
         }

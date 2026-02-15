5c7550154a995b575fded382836e51e80a09dfd7
Ehab Yasser <ehabyasser679@gmail.com>
2026-02-15 22:55:43 +0200

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     path("post/", BlogListView.as_view(), name= "posts"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name= "post_detail"),
     path("post/new/", BlogCreateView.as_view(), name= "post_new"),
-    path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name= "post_edit"),
+    path("post/<int:pk>/update/", BlogUpdateView.as_view(), name= "post_edit"),
     path("post/<int:pk>/delete/", BlogDeleteView.as_view(), name= "post_delete"),
 ]
+
 

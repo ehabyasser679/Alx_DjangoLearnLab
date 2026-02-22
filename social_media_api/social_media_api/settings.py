f9ee5997bded6957aaff2a1d1c80e2112d18a444
Ehab Yasser <ehabyasser679@gmail.com>
2026-02-23 01:16:45 +0200

    Update DEBUG setting in Django settings
    Changed DEBUG setting to False for production.

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b%$uen1^jv--nheo71i(i&z2+ir7o8@((s&)!7dsfv_ci1o%3+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 

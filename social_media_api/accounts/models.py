9363ebe81db96069eba18c40afb31901544b9468
Ehab Yasser <ehabyasser679@gmail.com>
2026-02-23 00:03:40 +0200

    Refactor Profile model and user signals

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.contrib.auth.models import AbstractUser
 
 
 class Profile(models.Model):
@@ -24,3 +25,4 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
+
